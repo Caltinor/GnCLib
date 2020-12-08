7a1cd125ea7bb72ce30f174a328c23f728c3f44c
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-12-08 17:44:51 -0500

    added MOD_ID

--- src/dicemc/gnclib/util/ComVars.java
+++ src/dicemc/gnclib/util/ComVars.java
@@ -4,4 +4,5 @@ import java.util.UUID;
 
 public class ComVars {
 	public static final UUID NIL = UUID.fromString("00000000-0000-0000-0000-000000000000");
+	public static final String MOD_ID = "gnc";
 }
