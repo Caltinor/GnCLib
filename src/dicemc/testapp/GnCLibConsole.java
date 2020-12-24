5bf64832a1df206148b99e63a69b85767a76e23d
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-12-24 13:40:45 -0500

    testing.  nothing special

--- src/dicemc/testapp/GnCLibConsole.java
+++ src/dicemc/testapp/GnCLibConsole.java
@@ -1,7 +1,17 @@
 package dicemc.testapp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import dicemc.gnclib.realestate.WhitelistEntry;
+
 public class GnCLibConsole {
 	public static void main(String []args){
-		System.out.println("Hello World");
+		Map<String, WhitelistEntry> whitelist = new HashMap<String, WhitelistEntry>();
+		whitelist.put("block", new WhitelistEntry(true, true));
+		whitelist.computeIfAbsent("block", key -> new WhitelistEntry()).setCanBreak(false);
+		System.out.println(String.valueOf(whitelist.get("block").getCanBreak())+":"+
+				String.valueOf(whitelist.get("block").getCanInteract()));
 	}
+	
 }
--- src/dicemc/testapp/impl/GroupsImpl.java
+++ src/dicemc/testapp/impl/GroupsImpl.java
@@ -1,7 +1,17 @@
 package dicemc.testapp.impl;
 
+import java.util.Map;
+import java.util.UUID;
+
+import dicemc.gnclib.guilds.Guild;
 import dicemc.gnclib.guilds.ILogicGuilds;
 
 public class GroupsImpl implements ILogicGuilds{
+
+	@Override
+	public Map<UUID, Guild> getGuilds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
