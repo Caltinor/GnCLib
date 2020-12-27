bcd8005dd8522f72b8386a4686d70cf2ebcfd444
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-12-27 15:37:34 -0500

    added unique UUID method

--- src/dicemc/gnclib/util/ComVars.java
+++ src/dicemc/gnclib/util/ComVars.java
@@ -1,8 +1,16 @@
 package dicemc.gnclib.util;
 
+import java.util.Map;
 import java.util.UUID;
 
 public class ComVars {
 	public static final UUID NIL = UUID.fromString("00000000-0000-0000-0000-000000000000");
+	public static final UUID INV = UUID.fromString("00000000-0000-0000-0000-000000000001");
 	public static final String MOD_ID = "gnc";
+	
+	public static UUID unrepeatedUUIDs(Map<UUID, ? extends Object> map) {
+    	UUID out = UUID.randomUUID();
+    	if (map.get(out) == null) return out;
+    	else return unrepeatedUUIDs(map);
+    }
 }
