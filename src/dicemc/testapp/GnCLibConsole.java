a39f3e8a74a84fc0485dc60399ad46f03d557957
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-12-31 06:55:13 -0500

    testing

--- src/dicemc/testapp/GnCLibConsole.java
+++ src/dicemc/testapp/GnCLibConsole.java
@@ -1,31 +1,15 @@
 package dicemc.testapp;
 
-import java.nio.charset.Charset;
 import java.util.UUID;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
+import dicemc.gnclib.configs.ConfigCore;
 
 public class GnCLibConsole {
 	public static UUID id = UUID.randomUUID();
 	
 	public static void main(String []args) {
-		System.out.println(id.toString());
-		ByteBuf buffer = write(Unpooled.buffer());
-    	read(buffer);
-    	System.out.println(id.toString());
+		System.out.println(-(ConfigCore.GUILD_NAME_CHANGE_COST));
 		//javax.swing.SwingUtilities.invokeLater(new Runnable() {public void run() {ScreenRoot.openGUI();}});
-	}
-	
-	private static ByteBuf write(ByteBuf buf) {
-		buf.writeInt(id.toString().length());
-		buf.writeCharSequence(id.toString(), Charset.defaultCharset());
-		return buf;
-	}
-	
-	private static void read(ByteBuf buf) {
-		int len = buf.readInt();
-		id = UUID.fromString(buf.readCharSequence(len, Charset.defaultCharset()).toString());
 	}
 	
 }
