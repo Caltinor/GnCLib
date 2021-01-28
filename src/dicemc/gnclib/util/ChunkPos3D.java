973aadad79a3736750231f82112696a1e86c0755
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-01-28 18:25:12 -0500

    Finalized and tested constructor math

--- src/dicemc/gnclib/util/ChunkPos3D.java
+++ src/dicemc/gnclib/util/ChunkPos3D.java
@@ -2,6 +2,14 @@ package dicemc.gnclib.util;
 
 public class ChunkPos3D {
 	public int x, y, z;
+	private static final int xSize = 1 + doMath(smallestPower(30000000));
+	private static final int zSize = xSize;
+	private static final int ySize = 64 - xSize - zSize;
+	private static final long xComp = (1L << xSize) - 1L;
+	private static final long yComp = (1L << ySize) - 1L;
+	private static final long zComp = (1L << zSize) - 1L;
+	private static final int zInverseStart = ySize;
+	private static final int xInverseStart = ySize + zSize;
 	
 	public ChunkPos3D(int x, int y, int z) {
 		this.x = x;
@@ -10,13 +18,39 @@ public class ChunkPos3D {
 	}
 	
 	public ChunkPos3D(long pos) {
-		//TODO complete bitwise conversionS
+		this.x = (int)(pos << 64 - xInverseStart - xSize >> 64 - xSize);
+		this.y = (int)(pos << 64 - ySize >> 64 - ySize);
+		this.z = (int)(pos << 64 - zInverseStart - zSize >> 64 - zSize);
 	}
 	
 	public long toLong() {
-		//TODO flesh out
-		return 1L;
+		long i = 0L;
+	    i = i | ((long)x & xComp) << xInverseStart;
+	    i = i | ((long)y & yComp) << 0;
+	    return i | ((long)z & zComp) << zInverseStart;
 	}
 	
 	public static int chunkYFromAltitude(int altitude) {return altitude/16;}
+	
+	private static int doMath(int value) {
+		boolean powerOfTwo = true;
+		int innerValue = value;
+		if (!(value != 0 && (value & value - 1) == 0)) {
+		     innerValue =  smallestPower(value);
+		     powerOfTwo = false;
+		}
+		int[] posArray = new int[]{0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9};
+		int bitPos = posArray[(int)((long)innerValue * 125613361L >> 27) & 31];
+		return bitPos - (powerOfTwo ? 0 : 1);
+	}
+	
+	private static int smallestPower(int value) {
+		 int i = value - 1;
+	     i = i | i >> 1;
+	     i = i | i >> 2;
+	     i = i | i >> 4;
+	     i = i | i >> 8;
+	     i = i | i >> 16;
+	     return  i + 1;
+	}
 }
